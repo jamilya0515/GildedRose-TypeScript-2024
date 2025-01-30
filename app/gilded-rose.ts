31b64eb9073bb016166b8f0931736f5495a705bc
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-01-30 16:17:18 +0000

    Adding functions

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,6 +17,61 @@ export class GildedRose {
     this.items = items;
   }
 
+  checkItemName(currentItemName: string) {
+    
+  }
+  isQualityZero(currentItemQuality: number) {
+    //if true -> return currentItemQuality
+    //if false, then call degradeQuality()
+    // return currentItemQuality
+  }
+
+  isQualityMax(currentItem: Item, incrementQuality: number) {
+    //if true -> return currentItemQuality --no cha
+    if (currentItem.quality)
+    //if false -> call degradeQuality || allow degradeQuality() to proceed?
+  }
+
+  isBackstagePass(currentItem: Item) {
+    if (currentItem.sellIn >= 6 && currentItem.sellIn <= 10) {
+      currentItem.quality += 2;
+    }
+    else if (currentItem.sellIn >= 0 && currentItem.sellIn <= 5) {
+      currentItem.quality += 3;
+    }
+    else (currentItem.sellIn > 10) {
+      this.increaseByOne(currentItem);
+    }
+    return currentItem;
+  }
+
+  increaseByOne(currentItem: Item) {
+    currentItem.quality += 1;
+    return currentItem;
+  }
+
+  degradeQuality(currentItem) {
+    if (currentItem.name == 'Aged Brie') {
+      if (!this.isQualityMax(currentItem, 1)) {
+        false
+      }
+      else 
+        this.increaseByOne(currentItem);
+    }
+
+    else if (currentItemName == 'Sulfuras, Hand of Ragnaros') {
+      this.items[i].quality = this.items[i].quality;
+    }
+    else if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      this.isBackstagePass(currentItem);
+      }
+    }
+    else
+      this.items[i].quality -= 1;
+
+    return this.items[i].quality;
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
